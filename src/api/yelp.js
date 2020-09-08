b93d018a914d5559e535afd38e3b4c47875bae5b
Adebayo <daramola.adebayo@yahoo.com>
2020-09-08 13:41:22 +0100

    hide api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const key = process.env.API_KEY
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer 5z1xdb84hX5kJjW8lBrjHxDxK287oiEUSQ7UHYOMyTSMOzYo_6Iz4gMuyzr3IiCyvswBtfvN4nZu7HR98S58OmRFylBrlwQrpLiMBnXdNSmlgjZkRoBMDGx7WScLX3Yx'
+    Authorization: `Bearer ${key}`
   }
 });
 
